3815601bfbc4cc75d9c47ce9f1fd82f5c64ddc12
Mikhail Kuznetsov <mishakuznex@gmail.com>
2022-12-09 17:59:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// function changeImgSrc(anything){
+//     document.querySelector('.imggg').src =  anything;
+// }
+
+VanillaTilt.init(document.querySelectorAll(".imgBox"), {
+    max: 15,
+    speed: 400
+});
